62a16e499edc6a15ab968ae6ed1459999039b788
matheus142 <matheusmw683@gmail.com>
2021-08-26 19:11:54 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Eu sou Rico'),
+          backgroundColor: Colors.deepPurpleAccent[400],
+        ),
+        backgroundColor: Colors.grey[900],
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
